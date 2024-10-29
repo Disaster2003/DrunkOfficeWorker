5a8b045b515dc68957bede133e59cf154cebffad
Disaster <Disaster20031017@gmail.com>
2024-10-29 10:40:02 +0900

    Adjust

---
+++ Assets/_yoshino/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField, Header("初期位置")]
+    private Vector3 positionInitialize;
+    [SerializeField, Header("目標地点")]
+    private float position_xGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期配置
+        transform.position = positionInitialize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x >= position_xGoal)
+        {
+            // 初期位置へ
+            transform.position = positionInitialize;
+        }
+
+        // 背景のスクロール
+        transform.Translate(10 * Time.deltaTime, 0, 0);
+    }
+}
