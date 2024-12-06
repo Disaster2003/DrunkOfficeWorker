f7d45961b96dc3dfbe0e07aa1da9bf96880a6940
Disaster <Disaster20031017@gmail.com>
2024-12-06 13:47:37 +0900

    Update Progress.cs

--- Assets/2_Play/Scripts/Progress.cs
+++ Assets/2_Play/Scripts/Progress.cs
@@ -13,8 +13,10 @@ public class Progress : MonoBehaviour
 
     void Update()
     {
+        // nullチェック
         if (position_Goal == Vector3.zero)
         {
+            Debug.LogWarning("進捗の目標地点が未設定です");
             return;
         }
 
@@ -22,17 +24,18 @@ public class Progress : MonoBehaviour
         Vector3 positionTarget = new Vector3(-position_Goal.x + SpawnerPlay.GetCount * 100, transform.localPosition.y);
         Vector3 positionNew = Vector3.Lerp(transform.localPosition, positionTarget, Time.deltaTime);
 
-        //if(Vector3.Distance(transform.localPosition, positionTarget) < 0.1f)
+        //if (Vector3.Distance(transform.localPosition, positionTarget) < 0.1f)
         //{
-        // 小刻みに揺れる動きをPerlinNoiseで加える
-        shakeTime += Time.deltaTime * shakeFrequency;
-        float shakeOffset = Mathf.PerlinNoise(shakeTime, 0) * shakeAmplitude * 2f - shakeAmplitude; // -shakeAmplitude で揺れが-振幅~+振幅の範囲で小刻みに動く
-        positionNew.y += shakeOffset;
+        //    // 小刻みに揺れる動きをPerlinNoiseで加える
+        //    shakeTime += Time.deltaTime * shakeFrequency;
+        //    float shakeOffset = Mathf.PerlinNoise(shakeTime, 0) * shakeAmplitude * 2f - shakeAmplitude; // -shakeAmplitude で揺れが-振幅~+振幅の範囲で小刻みに動く
+        //    positionNew.y += shakeOffset;
 
-        // 新しい位置に揺れのオフセットを加える
-        positionNew.y += shakeOffset;
-
-        transform.localPosition = positionNew;
+        //    // 新しい位置に揺れのオフセットを加える
+        //    positionNew.y += shakeOffset;
         //}
+
+        // 結果を代入
+        transform.localPosition = positionNew;
     }
 }
